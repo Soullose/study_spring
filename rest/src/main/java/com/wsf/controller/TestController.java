bec7d5161d6d89fa0413d83b228c0fcbc29d03d0
soullose <soullose@qq.com>
2023-07-06 17:30:31 +0800

    ....

--- rest/src/main/java/com/wsf/controller/TestController.java
+++ rest/src/main/java/com/wsf/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.wsf.controller;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.wsf.entity.User;
+import com.wsf.repository.UserRepository;
+
 /**
  * open
  * SoulLose
@@ -21,4 +28,23 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("test")
 @RestController
 public class TestController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+
+        User user = new User();
+        user.setUserName("test");
+        user.setPassword("123456");
+        user.setIdCardNumber("333333333333333");
+        user.setPhoneNumber("13333333333");
+        user.setCreateTime(LocalDateTime.now());
+
+        userRepository.save(user);
+        log.info("test");
+        return ResponseEntity.ok().build();
+    }
+
 }
