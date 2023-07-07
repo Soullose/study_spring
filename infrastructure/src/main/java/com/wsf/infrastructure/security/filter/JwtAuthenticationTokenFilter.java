8be679995d5b824e19a599a68a62602c1771f89c
soullose <soullose@qq.com>
2023-07-07 23:19:23 +0800

    update JwtAuthenticationTokenFilter.java

--- infrastructure/src/main/java/com/wsf/infrastructure/security/filter/JwtAuthenticationTokenFilter.java
+++ infrastructure/src/main/java/com/wsf/infrastructure/security/filter/JwtAuthenticationTokenFilter.java
@@ -1,7 +1,30 @@
 package com.wsf.infrastructure.security.filter;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- *
+ * @author soullose
  */
-public class JwtAuthenticationTokenFilter {
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+	private static final String AUTHORIZATION = "authorization";
+
+	@Override protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain filterChain) throws ServletException, IOException {
+		final String authHeader = request.getHeader(AUTHORIZATION);
+
+		if (authHeader == null){
+			filterChain.doFilter(request,response);
+		}
+	}
 }
