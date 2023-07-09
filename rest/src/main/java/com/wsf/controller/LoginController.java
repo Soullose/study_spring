504566cadc99751eadd04be355459e5a22644e48
soullose <soullose@qq.com>
2023-07-09 11:17:58 +0800

    delete

--- rest/src/main/java/com/wsf/controller/LoginController.java
+++ rest/src/main/java/com/wsf/controller/LoginController.java
@@ -1,23 +1,23 @@
-package com.wsf.controller;
-
-import com.wsf.params.LoginUserParams;
-import com.wsf.infrastructure.service.LoginService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * open
- * SoulLose
- * 2022-04-27 19:01
- */
-@RequestMapping("/api/v1/")
-@RestController
-@Slf4j
-public class LoginController {
-
-}
+//package com.wsf.controller;
+//
+//import com.wsf.params.LoginUserParams;
+//import com.wsf.infrastructure.service.LoginService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * open
+// * SoulLose
+// * 2022-04-27 19:01
+// */
+//@RequestMapping("/api/v1/")
+//@RestController
+//@Slf4j
+//public class LoginController {
+//
+//}
