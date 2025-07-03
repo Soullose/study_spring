f599bc27b9f18418716d0646c0c76467c0d86bd9
WangSF <soullose@qq.com>
2025-07-03 17:59:10 +0800

    feat(infrastructure): 优化 HTTP 请求头处理
    - 解析请求 URI，获取真实路径
    - 添加日志输出，记录请求 URL 和真实路径
    - 新增方法 getIpAddr，用于获取客户端 IP地址
    - 在 channelRead 方法中添加 IP 地址的获取

--- infrastructure/src/main/java/com/wsf/infrastructure/websocket/HttpHeadersHandler.java
+++ infrastructure/src/main/java/com/wsf/infrastructure/websocket/HttpHeadersHandler.java
@@ -1,23 +1,59 @@
 package com.wsf.infrastructure.websocket;
 
+import java.net.URI;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
 
 public class HttpHeadersHandler extends ChannelInboundHandlerAdapter {
     private static final Logger log = LoggerFactory.getLogger(HttpHeadersHandler.class);
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        log.error("HttpHeadersHandler:{}", msg);
+        log.error("HttpHeadersHandler: {}", msg);
         if (msg instanceof FullHttpRequest request) {
+            URI uri = new URI(request.uri());
+            String realPath = uri.getPath();
+            
             log.debug("request-url:{}", request.uri());
+            log.debug("real-path:{}", realPath);
+            
+            HttpHeaders headers = request.headers();
+            String ip = getIpAddr(request);
+            log.debug("ip: {}", ip);
+            
             ctx.pipeline().remove(this);
             ctx.fireChannelRead(request);
-        }else {
+        } else {
             ctx.fireChannelRead(msg);
         }
+    }
+
+
+    public static String getIpAddr(FullHttpRequest request) {
+        if (request == null) {
+            return "unknown";
+        }
+        HttpHeaders headers = request.headers();
+        String ip = headers.get("x-forwarded-for");
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = headers.get("Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = headers.get("X-Forwarded-For");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = headers.get("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = headers.get("X-Real-IP");
+        }
+
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : "";
     }
 }
