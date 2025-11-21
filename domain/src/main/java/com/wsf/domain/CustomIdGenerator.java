7fd2f6b65bde3b520b4dc37705788bdbebbc183f
WangSF <soullose@qq.com>
2025-11-21 10:32:11 +0800

    refactor(id-generator): 使用GUID v7替代UUID生成策略
    重构自定义ID生成器，从基于时间排序的UUID生成改为使用GUID v7标准。主要变更包括：
    - 使用SecureRandom替代默认RandomGenerator提高安全性
    - 引入GUID v7生成策略，结合当前时间和安全随机数
    - 更新Maven依赖配置，添加java-uuid-generator依赖
    - 保留原有代码为注释以便参考

--- domain/src/main/java/com/wsf/domain/CustomIdGenerator.java
+++ domain/src/main/java/com/wsf/domain/CustomIdGenerator.java
@@ -1,22 +1,27 @@
 package com.wsf.domain;
 
 import java.io.Serializable;
-import java.util.random.RandomGenerator;
+import java.security.SecureRandom;
+import java.time.Instant;
 
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
-import com.github.f4b6a3.uuid.factory.standard.TimeOrderedEpochFactory;
+import com.github.f4b6a3.uuid.alt.GUID;
 
 ///主键生成策略
 public class CustomIdGenerator implements IdentifierGenerator {
 
 	@Override
 	public Serializable generate(SharedSessionContractImplementor arg0, Object arg1) throws HibernateException {
-		RandomGenerator random = RandomGenerator.getDefault();
-		TimeOrderedEpochFactory factory = new TimeOrderedEpochFactory(random::nextLong);
-		return factory.create().toString();
+//		RandomGenerator random = RandomGenerator.getDefault();
+//		TimeOrderedEpochFactory factory = new TimeOrderedEpochFactory(random::nextLong);
+		SecureRandom secureRandom = new SecureRandom();
+		GUID guid = GUID.v7(Instant.now(), secureRandom);
+//		UUID uuid = UuidCreator.getTimeOrderedEpochPlus1();
+		return guid.toString();
+//		return factory.create().toString();
 	}
 
 }
