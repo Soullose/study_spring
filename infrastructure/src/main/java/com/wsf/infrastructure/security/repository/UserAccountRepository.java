13e6120e379d79b95908076068037db80bf0a660
WangSF <soullose@qq.com>
2026-01-02 22:41:49 +0800

    feat(security): 添加Querydsl支持到用户账户仓库
    在UserAccountRepository中扩展QuerydslPredicateExecutor接口，支持Querydsl动态查询功能，同时优化了代码格式和导入语句顺序。

--- infrastructure/src/main/java/com/wsf/infrastructure/security/repository/UserAccountRepository.java
+++ infrastructure/src/main/java/com/wsf/infrastructure/security/repository/UserAccountRepository.java
@@ -4,13 +4,18 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.wsf.domain.model.entity.UserAccount;
 
 @Repository
-public interface UserAccountRepository extends JpaRepository<UserAccount, String>, JpaSpecificationExecutor<UserAccount> {
+public interface UserAccountRepository
+    extends
+      JpaRepository<UserAccount, String>,
+      JpaSpecificationExecutor<UserAccount>,
+      QuerydslPredicateExecutor<UserAccount> {
 
-    ///根据用户名查询UserAccount
-    Optional<UserAccount> findByUsername(String username);
+  /// 根据用户名查询UserAccount
+  Optional<UserAccount> findByUsername(String username);
 }
